b8980039656dc988a63a00194d3fe4d264c44183
Thiago Lelis <thiago.lelis@cravehq.com>
2024-03-23 13:03:43 -0300

    feat: implement the routes logic and create a service to implement the logic

---
+++ api/game_services.py
@@ -0,0 +1,23 @@
+import uuid
+
+
+# This is the dictionary that will perform the function of a database
+# Using a database is recommended
+# However, due to the short implementation time, I decided to store the games in memory
+games = {}
+
+
+def create_game(size=3):
+    pass
+
+
+def make_move(game_id, data):
+    pass
+
+
+def get_moves(game_id):
+    pass
+
+
+def get_games(game_id):
+    pass
--- api/routes.py
+++ api/routes.py
@@ -1,3 +1,24 @@
 from flask import Blueprint, jsonify, request
 
-game_blueprint = Blueprint("game", __name__, url_prefix='/api/v1')
+game_blueprint = Blueprint("game", __name__, url_prefix="/api/v1")
+
+
+@game_blueprint.route("/games", methods=["POST"])
+def new_game_route():
+    return create_game()
+
+
+@game_blueprint.route("/games/<string:game_id>/moves", methods=["POST"])
+def make_move_route(game_id):
+    data = request.get_json()
+    return make_move(game_id, data)
+
+
+@game_blueprint.route("/games/<string:game_id>/moves", methods=["GET"])
+def get_moves_route(game_id):
+    return get_moves(game_id)
+
+
+@game_blueprint.route("/games/<string:game_id>", methods=["GET"])
+def get_games_route(game_id):
+    return get_games(game_id)
